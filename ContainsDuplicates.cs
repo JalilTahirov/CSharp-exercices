7df2331b17d6ce89519f65106ad9bd3aca7efe87
JalilTahirov <jalil.tahirov@outlook.com>
2026-02-14 15:36:31 +0100

    Added ContainsDuplicates

---
+++ ContainsDuplicates.cs
@@ -0,0 +1,27 @@
+void Main()
+{
+	var myNums = new int[] {1,2,3, 1};
+	var res = ContainsDuplicates(myNums);
+	Console.WriteLine(res);
+}
+
+// You can define other methods, fields, classes and namespaces here
+public static bool ContainsDuplicates(int[] nums)
+{
+	var un = new HashSet<int>();
+	// for each value check if it contains in hashset if no add it there
+	for(int i = 0; i < nums.Length; i++)
+	{
+		if(un.Contains(nums[i]))
+		{
+			return true;
+		}
+		else
+		{
+			un.Add(nums[i]);
+		}
+	
+	}
+	
+    return false;
+}
