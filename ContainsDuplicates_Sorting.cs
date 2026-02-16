99ae54a70dcc57fef18173bc03876908f7def21e
JalilTahirov <jalil.tahirov@outlook.com>
2026-02-16 05:08:21 +0100

    sorted array

---
+++ ContainsDuplicates_Sorting.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+void Main()
+{
+	var nums  = new int[] {1,2,3,1,4,5};
+	Console.WriteLine(ContainsDuplicates(nums));
+}
+
+// You can define other methods, fields, classes and namespaces here
+
+public bool ContainsDuplicates(int[] nums)
+{
+        nums.Sort();
+		Console.WriteLine(nums);
+        for(int i = 1; i<nums.Length; i++ )
+        {
+            if(nums[i] == nums[i-1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+}
+
+
+
+}
